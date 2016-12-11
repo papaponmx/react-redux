8c2936c5d58f18d6aac4786b4b0b9839de311c37
Jaime Rios <jaimemadrigalrios@gmail.com>
2016-12-10 21:23:35 -0600

    CoursesPage added

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,7 +7,8 @@ const Header = () => {
         <nav>
             <IndexLink to ="/" activeClassName="active">Home</IndexLink>
             {" | "}
-            <Link to ="/about" activeClassName="active">About</Link>
+            <Link to ="/about" activeClassName="active">About</Link>            {" | "}
+            <Link to ="/courses" activeClassName="active">Courses</Link>
         </nav>
     );
 };
